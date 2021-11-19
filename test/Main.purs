bb437ef354c009dcf5a53001b5c12474c7c42665
James Collier <james.collier@vib.be>
2021-11-20 00:05:43 +0100

    Formatting and fix all the tests

--- test/Main.purs
+++ test/Main.purs
@@ -25,107 +25,95 @@ graph :: Phylogeny -> G.Graph NodeIdentifier PNode
 graph phy = un Network phy.network
 
 nodeName :: PNode -> Tuple String Number
-nodeName (PNode{name, branchLength}) = (name /\ branchLength)
+nodeName (PNode { name, branchLength }) = (name /\ branchLength)
 
 nameNodes :: Phylogeny -> Array (Tuple String Number)
 nameNodes phy =
   A.fromFoldable $ nodeName <$> (L.catMaybes $ flip G.lookup g <$> G.topologicalSort g)
   where
-    g = graph phy
+  g = graph phy
 
 expectNNodes :: forall m. MonadThrow Error m => String -> Int -> m Unit
 expectNNodes input n =
   (L.length <<< G.vertices <<< graph <$> parseNewick input)
     `shouldEqual`
-    Right n
+      Right n
 
 expectNames :: forall m. MonadThrow Error m => String -> Array (Tuple String Number) -> m Unit
 expectNames input names =
   (nameNodes <$> parseNewick input)
     `shouldEqual`
-    Right names
-  
+      Right names
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [consoleReporter] do
+main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Parse Newick" do
     it "Parses an empty sexpr" do
       expectNNodes "();" 2
 
     it "Parses a single node" do
-      expectNames "A;" [("A" /\ 0.0)]
+      expectNames "A;" [ ("A" /\ 0.0) ]
 
     it "Parses a singleton sexpr" do
-      expectNames "(A);" [("" /\ 0.0), ("A" /\ 0.0)]
+      expectNames "(A);" [ ("" /\ 0.0), ("A" /\ 0.0) ]
 
     it "Parses a string name" do
-      expectNames "(Hello,World);" [("" /\ 0.0), ("World" /\ 0.0), ("Hello" /\ 0.0)]
+      expectNames "(Hello,World);" [ ("" /\ 0.0), ("World" /\ 0.0), ("Hello" /\ 0.0) ]
 
     it "Parses a sexpr with spaces" do
-      expectNames "( A, B ,  C );" [("" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0)]
+      expectNames "( A, B ,  C );" [ ("" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0) ]
 
     it "Parses a single node with a branch length" do
-      expectNames "A:1.1;" [("A" /\ 1.1)]
+      expectNames "A:1.1;" [ ("A" /\ 1.1) ]
 
     it "Parses a list with 3 unnamed nodes" do
-      expectNames "(,,);" [("" /\ 0.0), ("" /\ 0.0), ("" /\ 0.0), ("" /\ 0.0)]
+      expectNames "(,,);" [ ("" /\ 0.0), ("" /\ 0.0), ("" /\ 0.0), ("" /\ 0.0) ]
 
---     it "Parses a nested list with unnamed nodes" do
---       parseNewick "(,,(,));" `shouldEqual` Right (Internal ("" /\ 0.0) [Leaf ("" /\ 0.0), Leaf ("" /\ 0.0), Internal ("" /\ 0.0) [Leaf ("" /\ 0.0), Leaf ("" /\ 0.0)]])
+    it "Parses a nested list with unnamed nodes" do
+      expectNNodes "(,,(,));" 6
 
---     it "Parses a nested list with named nodes" do
---       parseNewick "(A,B,(C,D));" `shouldEqual` Right (Internal ("" /\ 0.0) [Leaf ("A" /\ 0.0), Leaf ("B" /\ 0.0), Internal ("" /\ 0.0) [Leaf ("C" /\ 0.0), Leaf ("D" /\ 0.0)]])
+    it "Parses a nested list with named nodes" do
+      expectNames "(A,B,(C,D));" [ ("" /\ 0.0), ("" /\ 0.0), ("D" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0) ]
 
---     it "Parses a rooted and nested tree with all named nodes" do
---       parseNewick "(A,(B)C)D;" `shouldEqual` Right (Internal ("D" /\ 0.0) [Leaf ("A" /\ 0.0), Internal ("C" /\ 0.0) [Leaf ("B" /\ 0.0)]])
+    it "Parses a rooted and nested tree with all named nodes" do
+      expectNames "(A,(B)C)D;" [ ("D" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0) ]
 
---     it "Parses a rooted and nested tree with all named nodes and branch lengths" do
---       parseNewick "(A:1.0,(B:1.5)C:0.9)D:5.6;"
---         `shouldEqual`
---         Right (Internal ("D" /\ 5.6) [Leaf ("A" /\ 1.0), Internal ("C" /\ 0.9) [Leaf ("B" /\ 1.5)]])
+    it "Parses a rooted and nested tree with all named nodes and branch lengths" do
+      expectNames "(A:1.0,(B:1.5)C:0.9)D:5.6;"
+        [ ("D" /\ 5.6), ("C" /\ 0.9), ("B" /\ 1.5), ("A" /\ 1.0) ]
 
---     it "Parses a rooted and nested tree with all no named nodes but all branch lengths" do
---       parseNewick "(:1.0,(:1.5):0.9):5.6;"
---         `shouldEqual`
---         Right (Internal ("" /\ 5.6) [Leaf ("" /\ 1.0), Internal ("" /\ 0.9) [Leaf ("" /\ 1.5)]])
+    it "Parses a rooted and nested tree with no named nodes but all branch lengths" do
+      expectNames "(:1.0,(:1.5):0.9):5.6;"
+        [ ("" /\ 5.6), ("" /\ 0.9), ("" /\ 1.5), ("" /\ 1.0) ]
 
---   describe "Parse Newick wikipedia examples" do
---     it "Parses no nodes are named" do
---       parseNewick "(,,(,));"
---         `shouldEqual`
---         Right (Internal ("" /\ 0.0) [Leaf ("" /\ 0.0), Leaf ("" /\ 0.0), Internal ("" /\ 0.0) [Leaf ("" /\ 0.0), Leaf ("" /\ 0.0)]])
+  describe "Parse Newick wikipedia examples" do
+    it "Parses no nodes are named" do
+      expectNNodes "(,,(,));" 6
 
---     it "Parses leaf nodes are named" do
---       parseNewick "(A,B,(C,D));"
---         `shouldEqual`
---         Right (Internal ("" /\ 0.0) [Leaf ("A" /\ 0.0), Leaf ("B" /\ 0.0), Internal ("" /\ 0.0) [Leaf ("C" /\ 0.0), Leaf ("D" /\ 0.0)]])
+    it "Parses leaf nodes are named" do
+      expectNames "(A,B,(C,D));"
+        [ ("" /\ 0.0), ("" /\ 0.0), ("D" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0) ]
 
---     it "Parses all nodes are named" do
---       parseNewick "(A,B,(C,D)E)F;"
---         `shouldEqual`
---         Right (Internal ("F" /\ 0.0) [Leaf ("A" /\ 0.0), Leaf ("B" /\ 0.0), Internal ("E" /\ 0.0) [Leaf ("C" /\ 0.0), Leaf ("D" /\ 0.0)]])
+    it "Parses all nodes are named" do
+      expectNames "(A,B,(C,D)E)F;"
+        [ ("F" /\ 0.0), ("E" /\ 0.0), ("D" /\ 0.0), ("C" /\ 0.0), ("B" /\ 0.0), ("A" /\ 0.0) ]
 
---     it "Parses all but root node have a distance to parent" do
---       parseNewick "(:0.1,:0.2,(:0.3,:0.4):0.5);"
---         `shouldEqual`
---         Right (Internal ("" /\ 0.0) [Leaf ("" /\ 0.1), Leaf ("" /\ 0.2), Internal ("" /\ 0.5) [Leaf ("" /\ 0.3), Leaf ("" /\ 0.4)]])
+    it "Parses all but root node have a distance to parent" do
+      expectNames "(:0.1,:0.2,(:0.3,:0.4):0.5);"
+        [ ("" /\ 0.0), ("" /\ 0.5), ("" /\ 0.4), ("" /\ 0.3), ("" /\ 0.2), ("" /\ 0.1) ]
 
---     it "Parses all have a distance to parent" do
---       parseNewick "(:0.1,:0.2,(:0.3,:0.4):0.5):0.0;"
---         `shouldEqual`
---         Right (Internal ("" /\ 0.0) [Leaf ("" /\ 0.1), Leaf ("" /\ 0.2), Internal ("" /\ 0.5) [Leaf ("" /\ 0.3), Leaf ("" /\ 0.4)]])
+    it "Parses all have a distance to parent" do
+      expectNames "(:0.1,:0.2,(:0.3,:0.4):0.5):0.0;"
+        [ ("" /\ 0.0), ("" /\ 0.5), ("" /\ 0.4), ("" /\ 0.3), ("" /\ 0.2), ("" /\ 0.1) ]
 
---     it "Parses distances and leaf names (popular)" do
---       parseNewick "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);"
---         `shouldEqual`
---         Right (Internal ("" /\ 0.0) [Leaf ("A" /\ 0.1), Leaf ("B" /\ 0.2), Internal ("" /\ 0.5) [Leaf ("C" /\ 0.3), Leaf ("D" /\ 0.4)]])
+    it "Parses distances and leaf names (popular)" do
+      expectNames "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);"
+        [ ("" /\ 0.0), ("" /\ 0.5), ("D" /\ 0.4), ("C" /\ 0.3), ("B" /\ 0.2), ("A" /\ 0.1) ]
 
---     it "Parses distances and all names" do
---       parseNewick "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;"
---         `shouldEqual`
---         Right (Internal ("F" /\ 0.0) [Leaf ("A" /\ 0.1), Leaf ("B" /\ 0.2), Internal ("E" /\ 0.5) [Leaf ("C" /\ 0.3), Leaf ("D" /\ 0.4)]])
+    it "Parses distances and all names" do
+      expectNames "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;"
+        [ ("F" /\ 0.0), ("E" /\ 0.5), ("D" /\ 0.4), ("C" /\ 0.3), ("B" /\ 0.2), ("A" /\ 0.1) ]
 
---     it "Parses a tree rooted on a leaf node (rare)" do
---       parseNewick "((B:0.2,(C:0.3,D:0.4)E:0.5)F:0.1)A;"
---         `shouldEqual`
---         Right (Internal ("A" /\ 0.0) [Internal ("F" /\ 0.1) [Leaf ("B" /\ 0.2), Internal ("E" /\ 0.5) [Leaf ("C" /\ 0.3), Leaf ("D" /\ 0.4)]]])
+    it "Parses a tree rooted on a leaf node (rare)" do
+      expectNames "((B:0.2,(C:0.3,D:0.4)E:0.5)F:0.1)A;"
+        [ ("A" /\ 0.0), ("F" /\ 0.1), ("E" /\ 0.5), ("D" /\ 0.4), ("C" /\ 0.3), ("B" /\ 0.2) ]
