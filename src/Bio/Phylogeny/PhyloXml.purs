50d10f4b79c7d3d7e025529a5109385f0c2193af
James Collier <james.collier@vib.be>
2021-12-13 21:54:29 +0100

    Can now parse basic XML

--- src/Bio/Phylogeny/PhyloXml.purs
+++ src/Bio/Phylogeny/PhyloXml.purs
@@ -2,20 +2,34 @@ module Bio.Phylogeny.PhyloXml where
 
 import Prelude hiding (between)
 
-import Bio.Phylogeny.Types (NodeType(..), PNode(..), Parser, Phylogeny, Tree(..), interpretIntermediate)
+import Bio.Phylogeny.Types (Attribute(..), NodeType(..), PNode(..), Parser, Phylogeny, Tree(..), interpretIntermediate)
+import Control.Alt ((<|>))
+import Control.Lazy (fix)
+import Debug (trace)
+import Data.Array as A
 import Data.Bifunctor (lmap)
 import Data.Either (Either)
 import Data.Map as M
 import Data.Maybe (Maybe(..))
+import Data.String.CodeUnits (fromCharArray)
+import Data.Tuple (Tuple)
+import Data.Tuple.Nested ((/\))
 import Text.Parsing.Parser (runParser)
-import Text.Parsing.Parser.Combinators (between)
-import Text.Parsing.Parser.String (string)
+import Text.Parsing.Parser.Combinators (between, choice, lookAhead, many1, sepBy, try)
+import Text.Parsing.Parser.String (char, oneOf, skipSpaces, string)
+import Text.Parsing.Parser.Token (alphaNum, space)
 
-parsePhyloXml :: String -> Either String Phylogeny
-parsePhyloXml input = lmap show $ runParser input phyloXmlParser
+type XmlNode =
+  { name :: String
+  , attributes :: M.Map String Attribute
+  , value :: Maybe String
+  }
+
+parsePhyloXml :: String -> Either String (Tree XmlNode)
+parsePhyloXml input = lmap show $ runParser input phyloxml -- phyloXmlParser
 
 phyloXmlParser :: Parser Phylogeny
-phyloXmlParser = interpretIntermediate <$> xmltag "phyloxml" (pure $ Leaf pnode)
+phyloXmlParser = interpretIntermediate <$> (pure $ Leaf pnode)
   where
   pnode :: PNode
   pnode = PNode
@@ -26,11 +40,199 @@ phyloXmlParser = interpretIntermediate <$> xmltag "phyloxml" (pure $ Leaf pnode)
     , attributes: M.empty
     }
 
-xmlopen :: String -> Parser Unit
-xmlopen tag = (string $ "<" <> tag <> ">") *> pure unit
+nameChar :: Parser Char
+nameChar = choice [ alphaNum, oneOf [ '.', '-', '_', ':' ] ]
 
-xmlclose :: String -> Parser Unit
-xmlclose tag = (string $ "</" <> tag <> ">") *> pure unit
+name :: Parser String
+name = fromCharArray <<< A.fromFoldable <$> many1 nameChar
 
-xmltag :: forall a. String -> Parser a -> Parser a
-xmltag tag inner = between (xmlopen tag) (xmlclose tag) inner
+extraChars :: Parser Char
+extraChars = oneOf
+  [ '!'
+  , '#'
+  , '$'
+  , '%'
+  , '&'
+  , '\''
+  , '('
+  , ')'
+  , '*'
+  , '+'
+  , ','
+  , '-'
+  , '.'
+  , '/'
+  , ':'
+  , ';'
+  , '='
+  , '?'
+  , '@'
+  , '['
+  , '\\'
+  , ']'
+  , '^'
+  , '_'
+  , '`'
+  , '{'
+  , '|'
+  , '}'
+  , '~'
+  , '¡'
+  , '¢'
+  , '£'
+  , '¤'
+  , '¥'
+  , '¦'
+  , '§'
+  , '¨'
+  , '©'
+  , 'ª'
+  , '«'
+  , '¬'
+  , '®'
+  , '¯'
+  , '°'
+  , '±'
+  , '²'
+  , '³'
+  , '´'
+  , 'µ'
+  , '¶'
+  , '·'
+  , '¸'
+  , '¹'
+  , 'º'
+  , '»'
+  , '¼'
+  , '½'
+  , '¾'
+  , '¿'
+  , 'À'
+  , 'Á'
+  , 'Â'
+  , 'Ã'
+  , 'Ä'
+  , 'Å'
+  , 'Æ'
+  , 'Ç'
+  , 'È'
+  , 'É'
+  , 'Ê'
+  , 'Ë'
+  , 'Ì'
+  , 'Í'
+  , 'Î'
+  , 'Ï'
+  , 'Ð'
+  , 'Ñ'
+  , 'Ò'
+  , 'Ó'
+  , 'Ô'
+  , 'Õ'
+  , 'Ö'
+  , '×'
+  , 'Ø'
+  , 'Ù'
+  , 'Ú'
+  , 'Û'
+  , 'Ü'
+  , 'Ý'
+  , 'Þ'
+  , 'ß'
+  , 'à'
+  , 'á'
+  , 'â'
+  , 'ã'
+  , 'ä'
+  , 'å'
+  , 'æ'
+  , 'ç'
+  , 'è'
+  , 'é'
+  , 'ê'
+  , 'ë'
+  , 'ì'
+  , 'í'
+  , 'î'
+  , 'ï'
+  , 'ð'
+  , 'ñ'
+  , 'ò'
+  , 'ó'
+  , 'ô'
+  , 'õ'
+  , 'ö'
+  , '÷'
+  , 'ø'
+  , 'ù'
+  , 'ú'
+  , 'û'
+  , 'ü'
+  , 'ý'
+  , 'þ'
+  , 'ÿ'
+  ]
+
+value :: Parser String
+value =
+  fromCharArray <<< A.fromFoldable <$> A.many (alphaNum <|> space <|> extraChars)
+
+attribute :: Parser (Tuple String Attribute)
+attribute = do
+  key <- name
+  _ <- char '='
+  val <- between (char '"') (char '"') value
+  pure (key /\ Text val)
+
+open :: Parser (Tuple String (M.Map String Attribute))
+open = do
+  skipSpaces
+  _ <- char '<'
+  skipSpaces
+  tag <- name
+  skipSpaces
+  attrs <- A.fromFoldable <$> attribute `sepBy` (char ' ')
+  skipSpaces
+  _ <- char '>'
+  pure $ (tag /\ M.fromFoldable attrs)
+
+close :: String -> Parser Unit
+close tag = string "</" *> skipSpaces *> string tag *> skipSpaces *> string ">" *> pure unit
+
+element :: Parser (Tree XmlNode) -> Parser (Tree XmlNode)
+element inner = choice
+  [ try emptyElementTag
+  , sTag inner
+  ]
+
+sTag :: Parser (Tree XmlNode) -> Parser (Tree XmlNode)
+sTag inner = do
+  (tag /\ attrs) <- open
+  subTree <- A.many (try inner)
+  val <- try value
+  _ <- close tag
+  let node = \theVal -> { name: tag, attributes: attrs, value: theVal }
+  if A.null subTree then
+    pure $ Leaf $ node $ Just val
+  else
+    pure $ Internal (node Nothing) subTree
+
+emptyElementTag :: Parser (Tree XmlNode)
+emptyElementTag = do
+  skipSpaces
+  _ <- char '<'
+  skipSpaces
+  tag <- name
+  skipSpaces
+  attrs <- A.many attribute
+  skipSpaces
+  _ <- string "/>"
+  pure $ Leaf { name: tag, attributes: M.fromFoldable attrs, value: Nothing }
+
+--(string $ "<" <> tag) *> skipSpaces *> inner <* string "/>"
+
+-- phyloxml :: Parser (Tree PNode)
+-- phyloxml = element
+
+phyloxml :: Parser (Tree XmlNode)
+phyloxml = fix element
