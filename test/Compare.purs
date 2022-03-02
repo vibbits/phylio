ce312cd02659a0b0099846c507d9f769e280cc53
James Collier <james.collier@vib.be>
2022-03-02 23:57:29 +0100

    Extend comparative tests

--- test/Compare.purs
+++ test/Compare.purs
@@ -2,7 +2,8 @@ module Test.Compare where
 
 import Prelude
 
-import Bio.Phylogeny (parsePhyloXml, parseNewick)
+import Bio.Phylogeny (Phylogeny(..), parsePhyloXml, parseNewick, dot, edges, vertices, roots)
+import Data.Array as A
 import Data.Map as M
 import Node.Encoding (Encoding(UTF8))
 import Node.FS.Aff (readTextFile)
@@ -17,30 +18,53 @@ specs = do
     it "Parses leaf nodes are named" do
       phyloxmlText <- readTextFile UTF8 $ testDir </> "leaf-nodes-named.xml"
 
-      (M.fromFoldable <<< nameNodes <$> parseNewick "(A,B,(C,D));")
+      (parseNewick "(A,B,(C,D));")
         `shouldEqual`
-          (M.fromFoldable <<< nameNodes <$> parsePhyloXml phyloxmlText)
+          (parsePhyloXml phyloxmlText)
 
     it "Parses all nodes are named" do
       phyloxmlText <- readTextFile UTF8 $ testDir </> "all-nodes-named.xml"
 
-      (M.fromFoldable <<< nameNodes <$> parseNewick "(A,B,(C,D)E)F;")
+      (parseNewick "(A,B,(C,D)E)F;")
         `shouldEqual`
-          (M.fromFoldable <<< nameNodes <$> parsePhyloXml phyloxmlText)
+          (parsePhyloXml phyloxmlText)
 
     it "Parses all but root node have a distance to parent" do
       phyloxmlText <- readTextFile UTF8 $ testDir </> "distances-and-leaf-names.xml"
 
-      (M.fromFoldable <<< nameNodes <$> parseNewick "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);")
+      (parseNewick "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);")
         `shouldEqual`
-          (M.fromFoldable <<< nameNodes <$> parsePhyloXml phyloxmlText)
+          (parsePhyloXml phyloxmlText)
 
   describe "Compare equivalent serialisations in different formats" do
-    it "Newick and PhyloXML deserialise to the same network" do
+    it "Phaeobacter Newick/PhyloXML" do
       newickText <- readTextFile UTF8 $ testDir </> "Phaeobacter.newick"
       phyloxmlText <- readTextFile UTF8 $ testDir </> "Phaeobacter.phyloxml"
 
-      (M.fromFoldable <<< nameNodes <$> parseNewick newickText)
+      (parseNewick newickText)
         `shouldEqual`
-          (M.fromFoldable <<< nameNodes <$> parsePhyloXml phyloxmlText)
+          (parsePhyloXml phyloxmlText)
 
+    it "Dicots 1 Newick/PhyloXML" do
+      newickText <- readTextFile UTF8 $ testDir </> "dicots1.newick"
+      phyloxmlText <- readTextFile UTF8 $ testDir </> "dicots1.xml"
+
+      (parseNewick newickText)
+        `shouldEqual`
+          (parsePhyloXml phyloxmlText)
+
+    it "Bacillus Newick/PhyloXML" do
+      newickText <- readTextFile UTF8 $ testDir </> "bacillus.newick"
+      phyloxmlText <- readTextFile UTF8 $ testDir </> "bacillus.xml"
+
+      (parseNewick newickText)
+        `shouldEqual`
+          (parsePhyloXml phyloxmlText)
+
+    it "Pangolin SARS-CoV-2 Newick/PhyloXML" do
+      newickText <- readTextFile UTF8 $ testDir </> "pangolin.newick"
+      phyloxmlText <- readTextFile UTF8 $ testDir </> "pangolin.xml"
+
+      (parseNewick newickText)
+        `shouldEqual`
+          (parsePhyloXml phyloxmlText)
