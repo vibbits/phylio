976933b4be9a8f75dcfd1825d69fa6fe5259a50c
James Collier <james.collier@vib.be>
2024-08-16 12:21:02 +0200

    Upgrade eslint and typescript-eslint
    * Typescript-eslint have changed configuration a lot

--- js/main.ts
+++ js/main.ts
@@ -4,8 +4,10 @@
 
 import {
   parse as parse_,
-  //@ts-ignore - This Purescript import should be safe
+  //@ts-expect-error This Purescript import should be safe
 } from "../output/JsLib/index.js";
+
+declare function parse_(text: string): unknown;
 
 export interface Metadata {
   name: string | undefined;
@@ -51,8 +53,27 @@ type Result<T> =
   | { tag: "error"; message: string }
   | { tag: "success"; value: T };
 
+function toPhylogenyResult(val: unknown): Result<Phylogeny> {
+  if (val === null || val === undefined) {
+    return { tag: "error", message: "Internal error: null" };
+  } else {
+    if (typeof val === "object" && "tag" in val) {
+      if (val.tag == "error" && "message" in val){
+        return val as Result<Phylogeny>;
+      } else if (val.tag === "success" && "value" in val) {
+        return val as Result<Phylogeny>;
+      } else {
+        return {tag: "error", message: "Internal error: not an expected result"};
+      }
+    } else {
+      return {tag: "error", message: "Internal error: not a result"};
+    }
+  }
+}
+
 export const parse = (text: string): Phylogeny => {
-  const res: Result<Phylogeny> = parse_(text);
+  const res: Result<Phylogeny> = toPhylogenyResult(parse_(text));
+
   if (res.tag === "error") {
     throw new Error(res.message);
   }
